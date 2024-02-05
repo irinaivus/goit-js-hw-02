63ddce870ae3733df305248f525a1a9e3cd96b81
Iryna Serhiienko <ivus0888@gmail.com>
2024-02-05 16:06:41 +0200

    changes-hw-02

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,13 @@
-
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-    let message;
-    if (totalPrice > customerCredits) {
-        message = `Insufficient funds!`
-    } else {
-        message = `You ordered ${quantity} droids worth ${totalPrice} credits!`
-    }
-     return message;
-};
-
-makeTransaction(5, 3000, 23000);
-makeTransaction(3, 1000, 15000);
-makeTransaction(10, 5000, 8000);
-makeTransaction(8, 2000, 10000);
-makeTransaction(10, 500, 5000);
-
+  const totalPrice = quantity * pricePerDroid;
+  let message;
+  if (totalPrice > customerCredits) {
+    message = `Insufficient funds!`;
+  } else {
+    message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
+  return message;
+}
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
